0d73e8359221d7159ec733451c1078d6948f16a5
GarySu <Gary.Su@hearst.com>
2021-03-18 16:58:21 -0400

    refactor to use spread operatot

--- challenges-frontend/src/components/ChallengeComponent.tsx
+++ challenges-frontend/src/components/ChallengeComponent.tsx
@@ -1,68 +1,70 @@
 import React, { useState, useEffect } from 'react';
 import ApiClient from '../services/ApiClient';
 
+const initValues = {
+    factorA: '',
+    factorB: '',
+    user: '',
+    message: '',
+    guess: 0,
+};
 export function ChallengeComponent() {
-    const [factorA, setFactorA] = useState('');
-    const [factorB, setFactorB] = useState('');
-    const [user, setUser] = useState('');
-    const [message, setMessage] = useState('');
-    const [guess, setGuess] = useState(0);
+    const [values, setValues] = useState(initValues);
+
     useEffect(() => {
         ApiClient.challenge().then((res) => {
             if (res.ok) {
                 res.json().then((json) => {
-                    setFactorA(json.factorA);
-                    setFactorB(json.factorB);
+                    setValues({
+                        ...values,
+                        factorA: json.factorA,
+                        factorB: json.factorB,
+                    });
                 });
-            } else setMessage('Error: server error or not available');
+            } else setValues({ ...values, message: 'Error: server error or not available' });
         });
-    }, [user]);
+    }, [values.user]);
+
     const handleSubmitResult = (e: React.SyntheticEvent) => {
         e.preventDefault();
-        ApiClient.sendGuess(user, factorA, factorB, guess).then((res) => {
+        ApiClient.sendGuess(values.user, values.factorA, values.factorB, values.guess).then((res) => {
             if (res.ok) {
                 res.json().then((json) => {
-                    if (json.correct) setMessage('Congratulations! Your guess is correct');
-                    else setMessage('Oops! Your guess ' + json.resultAttempt + ' is wrong, but keep playing!');
+                    if (json.correct) setValues({ ...values, message: 'Congratulations! Your guess is correct' });
+                    else
+                        setValues({
+                            ...values,
+                            message: 'Oops! Your guess ' + json.resultAttempt + ' is wrong, but keep playing!',
+                        });
                 });
             }
         });
     };
+
+    const handleChange = (e: React.changeEvent) => setValues({ ...values, [e.target.name]: e.target.value });
 
     return (
         <div>
             <div>
                 <h3>Your new challenge is</h3>
                 <h1>
-                    {factorA} x {factorB}
+                    {values.factorA} x {values.factorB}
                 </h1>
             </div>
             <form onSubmit={handleSubmitResult}>
                 <label>
                     Your alias:
-                    <input
-                        type="text"
-                        maxLength={12}
-                        name="user"
-                        value={user}
-                        onChange={(e) => setUser(e.target.value)}
-                    />
+                    <input type="text" maxLength={12} name="user" value={values.user} onChange={handleChange} />
                 </label>
                 <br />
                 <label>
                     Your guess:
-                    <input
-                        type="number"
-                        min="0"
-                        name="guess"
-                        value={guess}
-                        onChange={(e) => setGuess(e.target.value)}
-                    />
+                    <input type="number" min="0" name="guess" value={values.guess} onChange={handleChange} />
                 </label>
                 <br />
                 <input type="submit" value="Submit" />
             </form>
-            <h4>{message}</h4>
+            <h4>{values.message}</h4>
         </div>
     );
 }
