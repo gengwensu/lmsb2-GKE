155c73abcf4c2c5d19b1f1ddc8039514e789869c
GarySu <Gary.Su@hearst.com>
2021-05-13 12:00:05 -0400

    finish adding gamification

---
+++ gamification/src/main/java/com/gsucode/lmsb/gamification/game/LeaderBoardController.java
@@ -0,0 +1,21 @@
+package com.gsucode.lmsb.gamification.game;
+
+import com.gsucode.lmsb.gamification.game.domain.LeaderBoardRow;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/leaders")
+@RequiredArgsConstructor
+public class LeaderBoardController {
+    private final LeaderBoardService leaderBoardService;
+
+    @GetMapping
+    public List<LeaderBoardRow> getLeaderBoard(){
+        return leaderBoardService.getCurrentLeaderBoard();
+    }
+}
